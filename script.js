dc18cf6ed344bd1c7b7309493bf9bfd5763d6bd8
Methanblun <methanblun@gmail.com>
2023-12-27 11:37:49 +0000

    html restructure for align the text field and sumbmit button in the same line

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 
 
-var thebutton = "hhffh"
-console.log(thebutton)
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Fonction pour ajouter une tâche
+    function addTask() {
+        // Récupérer la valeur du champ de saisie
+        const taskInput = document.getElementById('taskInput');
+        const taskValue = taskInput.value;
+
+        // Vérifier si la valeur n'est pas vide
+        if (taskValue.trim() !== '') {
+            // Créer un nouvel élément li pour la tâche
+            const taskItem = document.createElement('li');
+            taskItem.textContent = taskValue;
+
+            // Ajouter la tâche à la liste des tâches (à adapter selon votre structure)
+            const tasksList = document.querySelector('.tasks-side'); // Sélectionnez votre liste de tâches
+            tasksList.appendChild(taskItem);
+
+            // Effacer le champ de saisie
+            taskInput.value = '';
+        }
+    }
+
+    // Associer la fonction addTask au clic sur le bouton
+    window.addTask = addTask;
+});
