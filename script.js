bd6cf67e4d62ad7898ecd8d8fef323fc705f8a84
Methanblun <methanblun@gmail.com>
2023-12-29 00:54:59 +0000

    function learning (addEventLIstener , event.key)

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
+// Utilisez une fonction qui s'exécute après le chargement du contenu
 
 
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Fonction pour ajouter une tâche
-    function addTask() {
-        // Récupérer la valeur du champ de saisie
-        const taskInput = document.getElementById('taskInput');
-        const taskValue = taskInput.value;
 
-        // Vérifier si la valeur n'est pas vide
-        if (taskValue.trim() !== '') {
-            // Créer un nouvel élément li pour la tâche
-            const taskItem = document.createElement('li');
-            taskItem.textContent = taskValue;
+let monbutton = document.getElementById("monBouton")
 
-            // Ajouter la tâche à la liste des tâches (à adapter selon votre structure)
-            const tasksList = document.querySelector('.tasks-side'); // Sélectionnez votre liste de tâches
-            tasksList.appendChild(taskItem);
 
-            // Effacer le champ de saisie
-            taskInput.value = '';
-        }
-    }
-
-    // Associer la fonction addTask au clic sur le bouton
-    window.addTask = addTask;
-});
+monbutton.addEventListener("click", function(){
+    console.log("tu mas clique")
+})
