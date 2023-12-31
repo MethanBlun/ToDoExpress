6b6e714338756dcc484f8187df56a29f08ff35ce
Methanblun <methanblun@gmail.com>
2023-12-31 16:20:52 +0000

    addTask function

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 
 
 
-let monbutton = document.getElementById("monBouton")
-
-
-monbutton.addEventListener("click", function(){
-    console.log("tu mas clique")
-})
+function addTask() {
+    // Récupérez la valeur de l'input
+    var taskValue = document.getElementById('taskInput').value;
+    // Vérifiez si l'input n'est pas vide
+    if (taskValue.trim() !== '') {
+      // Créez un nouvel élément li
+      var newTask = document.createElement('div');
+      // Ajoutez la valeur de la tâche à l'élément li
+      newTask.textContent = taskValue
+      var tasksList = document.getElementById('tasksList');
+    //  var tasksList = document.(".taskSide")
+      // Ajoutez l'élément li à la liste
+      tasksList.appendChild(newTask);
+      
+      document.getElementById('taskInput').value = '';
+    }
+  }
+  
+  
